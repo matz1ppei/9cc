02b373187872a43f1df1a72b813a001fc61b422e
Ippei Matsubara <matz1ppei@gmail.com>
2019-05-16 08:36:30 +0900

    !=を追加

--- 9cc.c
+++ 9cc.c
@@ -246,6 +246,11 @@ void gen(Node *node) {
     printf("  sete al\n");
     printf("  movzb rax, al\n");
     break;
+  case TK_NE:
+    printf("  cmp rax, rdi\n");
+    printf("  setne al\n");
+    printf("  movzb rax, al\n");
+    break;
   case '+':
     printf("  add rax, rdi\n");
     break;
